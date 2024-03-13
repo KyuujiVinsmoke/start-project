c5602ee0c7464be5d88b010564f3a084bbae4a8e
KyuujiVinsmoke <andreluizvelosocruz@gmail.com>
2024-03-13 00:28:47 -0300

    Initial commit

---
+++ app/Repositories/DocumentoRepository.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace App\Repositories;
+
+use App\Models\Documento;
+
+class DocumentoRepository extends Repository { 
+    public function __construct() {
+        parent::__construct(new Documento());
+    }   
+}
