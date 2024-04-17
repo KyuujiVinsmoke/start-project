78345a58582b81aea8729785dc0d703af71dab81
KyuujiVinsmoke <andreluizvelosocruz@gmail.com>
2024-04-17 00:11:52 -0300

    .

--- Docker/start-project/routes/web.php
+++ Docker/start-project/routes/web.php
@@ -22,44 +22,6 @@ Route::get('/simples', function () {
     return "<h1>Rota Simples</h1>";
 });
 
-Route::get('/parametro/{a}', function ($a) {
-    return "<h1>Parâmetro recebido: $a</h1>";    
-});
-
-Route::get('/parametros/{a}/{b}/{c}', function ($a, $b, $c) {
-    return "<h1>Parâmetros recebidos: $a / $b / $c</h1>";
-});
-
-Route::get('/opcionais/{a}/{b}/{c?}', function ($a, $b, $c=0) {
-    return "<h1>Parâmetros recebidos: $a / $b / $c</h1>";
-});
-
-Route::get('/regras/{a}/{b}', function ($a, $b) {
-    return "<h1>Parâmetros recebidos: $a / $b</h1>";
-})->where('a', '[0-9]+')->where('b', '[A-Za-z]+');
-
-Route::prefix('/agrupamento')->group(function() {
-    Route::get('/', function() {
-        return "<h1>Agrupamento: rota raiz</h1>";
-    })->name('agrupamento');
-
-    Route::get('/um', function() {
-        return "<h1>Agrupamento: rota um</h1>";
-    })->name('agrupamento.um');
-    
-    Route::get('/dois', function() {
-        return "<h1>Agrupamento: rota dois</h1>";
-    })->name('agrupamento.dois');
-});
-
-Route::get('/redirecionar', function () {
-    return redirect()->route('agrupamento');
-});
-
-Route::post('/add', function (Request $request) {
-    return "<h1>Requisição POST</h1>";
-});
-
 Route::resource('/eixo', 'App\Http\Controllers\EixoController');
 Route::resource('/nivel', 'App\Http\Controllers\NivelController');
 Route::resource('/resource', 'App\Http\Controllers\ResourceController');
